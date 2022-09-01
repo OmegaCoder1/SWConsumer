34aed18c05d912cf2bdfcd4fad5bacb534f81d52
omegacoder1 <omega@omega-XPS-8700>
2022-08-31 22:17:13 -0300

    essa eh a classe main

---
+++ src/main/java/br/uefs/ecomp/swConsumer/view/Main.java
@@ -0,0 +1,18 @@
+package br.uefs.ecomp.swConsumer.view;
+
+import br.uefs.ecomp.swConsumer.model.Hidrometro;
+
+
+/**
+ * Essa 'e a classe principal
+ * @author Robson Jones
+ */
+public class Main {
+    private Hidrometro hidro;
+    
+    public static void main(String args[])throws Exception{  
+        Hidrometro hidro = new Hidrometro(589, 10, 5, false);
+        
+        
+    }
+}  
